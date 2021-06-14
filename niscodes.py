a8de3ac5664d59e76f4da8f9296f2ecfc659ddb3
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 06:43:23 +0200

    Gebruik DictReader van de module csv
    Python heeft een aparte module om csv-bestanden te lezen. Het grote
    voordeel is dat we gebruik kunnen maken van de kolomheaders. De
    DictReader geeft een reeks van sleutel-waarde-paren terug. De sleutel
    is de veldnaam, de waarde is de waarde van het element.
    
    Bij het openen van het bestand, moeten we een extra optie meegeven:
    newline=''. (zie documentatie van de csv-module) In ons geval zal dat
    geen verschil maken. Maar voor csv-bestanden waarbij een veld een
    newline bevat, zouden we in de problemen kunnen komen omdat de open()-
    functie de regel in twee zal splitsen.
    
    Een csv-reader kan een newline in een veld herkennen. Daarom laten we
    afhandeling van newlines best over aan (in dit geval) de DictReader.
    
    Zie:
    - https://docs.python.org/3/library/csv.html
    - https://docs.python.org/3/library/csv.html#csv.DictReader

--- niscodes.py
+++ niscodes.py
@@ -1,9 +1,8 @@
+import csv
+
 BESTANDSNAAM = '../REFNIS_2019.csv'
 
-with open(BESTANDSNAAM, mode='r', encoding='utf_8_sig') as csvfile:
-    # We gebruiken '_' voor variabelen waarvan we de waarde niet gebruiken
-    # Op de vijfde regel staat de eerste echte gemeente
-    for _ in range(5):
-        regel = csvfile.readline()
-    items = regel.split(';')  # scheidingsteken is ';'
-    print(items[3], items[4])          # print de NIS-code en de naam van Anderlecht
+
+with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    print(reader.fieldnames)
