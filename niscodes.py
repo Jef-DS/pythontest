4d0c5989326ec8dd36ed6fed1656ee1269b8bec8
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 07:01:39 +0200

    Tel het aantal gemeenten(eerste probeersel)
    Het bestand bevat niet alleen de gemeenten, maar ook de gewesten, de
    provincincies en de arrondissementen. De NIS-codes van de niet-
    gemeenten eindigen allemaal op '000'. We controleren hier of we de
    informatie goed inlezen. Volgens de website van STATBEL zijn er 581
    gemeenten in België. In dit programma zijn het er 583. We zullen dus
    moeten uitzoeken waar we een fout hebben gemaakt. (of hebben we
    misschien 2 'geheime' gemeenten ontdekt?)
    
    Zie:
    - https://statbel.fgov.be/nl/over-statbel/methodologie/classificaties/geografie#toc_heading_3

--- niscodes.py
+++ niscodes.py
@@ -6,6 +6,13 @@ GEMEENTENAAM = 'Administratieve eenheden'
 
 with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
-    # de next() functie gaat naar het volgende item van de iterator
-    record = next(reader) 
-    print(record[NIS_CODE], record[GEMEENTENAAM])
+    totaal_aantal = 0
+    aantal_gemeenten = 0
+    for record in reader:
+        totaal_aantal = totaal_aantal + 1
+        if record[NIS_CODE][-3:] == '000':
+            print(record[NIS_CODE], record[GEMEENTENAAM])
+        else:
+            aantal_gemeenten = aantal_gemeenten + 1
+    print("Totaal:", totaal_aantal)
+    print("Gemeenten", aantal_gemeenten)
