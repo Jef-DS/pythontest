a94e7673b429fc78a80507acd8ebb3dbd9f56a62
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 07:28:38 +0200

    Zoek de NIS-code van een gemeente
    We maken een Dictionary object (gemeenten) in Python om een lijst van
    sleutel-waarde-paren te bewaren. Omdat we willen zoeken op gemeente-
    naam gebruiken we de gemeentenaam als sleutel. De NIS-code is de
    waarde. Om niet in de problemen te komen met het verschil in hoofd- en
    kleine letters, zetten we alle gemeenten om naar hoofdletters.
    
    Aan de if... elif... elif ...-structuur is er niets veranderd. Het zou
    efficiënter zijn wanneer we 1 if zouden schrijven (if is_gemeente:),
    maar op deze manier is het duidelijker waarom we sommige patronen uit-
    sluiten. Omdat we echter niets willen doen wanneer het geen gemeente-
    patroon is, gebruiken we het 'pass'-keyword. We moeten immers iets
    schrijven wat uitgevoerd kan worden door Python. Een commenaarlijn
    werkt niet.
    
    Zie:
    - https://docs.python.org/3/reference/simple_stmts.html#the-pass-statement

--- niscodes.py
+++ niscodes.py
@@ -6,27 +6,23 @@ GEMEENTENAAM = 'Administratieve eenheden'
 
 with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
-    totaal_aantal = 0
-    het_rijk = 0
-    aantal_gewesten = 0
-    aantal_provincies = 0
-    aantal_arrondissementen = 0
+    gemeenten = {} # initialiseer een dictionary
     aantal_gemeenten = 0
     for record in reader:
-        totaal_aantal = totaal_aantal + 1
         if record[NIS_CODE] == '01000':
-            het_rijk = het_rijk + 1
+            pass  # het rijk
         elif record[NIS_CODE][0] == '0':
-            aantal_gewesten = aantal_gewesten + 1
+            pass # een gewest
         elif record[NIS_CODE][-4:-1] == '000':
-            aantal_provincies = aantal_provincies + 1
+            pass # een provincie
         elif record[NIS_CODE][-3:] == '000':
-            aantal_arrondissementen = aantal_arrondissementen + 1
+            pass # een arrondissement
         else:
-            aantal_gemeenten = aantal_gemeenten + 1
-    print("Totaal:", totaal_aantal)
-    print("Het rijk:", het_rijk)
-    print("Aantal gewesten:", aantal_gewesten)
-    print("Aantal provincies: ", aantal_provincies)
-    print("Aantal arrondissementen: ", aantal_arrondissementen)
-    print("Aantal gemeenten:", aantal_gemeenten)
+            aantal_gemeenten += 1
+            nis_code = record[NIS_CODE]
+            gemeentenaam = record[GEMEENTENAAM].upper()
+            gemeenten[gemeentenaam] = nis_code
+    print(f"{aantal_gemeenten} gemeenten ingelezen")
+    naam = input("Geef gemeentenaam: ")
+    code = gemeenten[naam.upper()]
+    print(f"Gemeente {naam} heeft als NIS code {code}")
