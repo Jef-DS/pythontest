d270cf4f14c4505294eb1dacb30cb7cbc684d152
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 07:44:07 +0200

    Maak functie lees_nis_bestand()
    Om deze code later te kunnen gebruiken, is het best om een functie te
    maken. Het zou misschien logischer zijn om de naam van het bestand als
    argument mee te geven. Dat maakt de functie flexibeler.

--- niscodes.py
+++ niscodes.py
@@ -1,28 +1,32 @@
 import csv
 
-BESTANDSNAAM = '../REFNIS_2019.csv'
-NIS_CODE = 'Code NIS'
-GEMEENTENAAM = 'Administratieve eenheden'
+def lees_nis_bestand():
+    BESTANDSNAAM = '../REFNIS_2019.csv'
+    NIS_CODE = 'Code NIS'
+    GEMEENTENAAM = 'Administratieve eenheden'
 
-with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=';')
-    gemeenten = {} # initialiseer een dictionary
-    aantal_gemeenten = 0
-    for record in reader:
-        if record[NIS_CODE] == '01000':
-            pass  # het rijk
-        elif record[NIS_CODE][0] == '0':
-            pass # een gewest
-        elif record[NIS_CODE][-4:-1] == '000':
-            pass # een provincie
-        elif record[NIS_CODE][-3:] == '000':
-            pass # een arrondissement
-        else:
-            aantal_gemeenten += 1
-            nis_code = record[NIS_CODE]
-            gemeentenaam = record[GEMEENTENAAM].upper()
-            gemeenten[gemeentenaam] = nis_code
-    print(f"{aantal_gemeenten} gemeenten ingelezen")
-    naam = input("Geef gemeentenaam: ")
-    code = gemeenten[naam.upper()]
-    print(f"Gemeente {naam} heeft als NIS code {code}")
+    with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        gemeenten = {} # initialiseer een dictionary
+        aantal_gemeenten = 0
+        for record in reader:
+            if record[NIS_CODE] == '01000':
+                pass  # het rijk
+            elif record[NIS_CODE][0] == '0':
+                pass # een gewest
+            elif record[NIS_CODE][-4:-1] == '000':
+                pass # een provincie
+            elif record[NIS_CODE][-3:] == '000':
+                pass # een arrondissement
+            else:
+                aantal_gemeenten += 1
+                nis_code = record[NIS_CODE]
+                gemeentenaam = record[GEMEENTENAAM].upper()
+                gemeenten[gemeentenaam] = nis_code
+        print(f"{aantal_gemeenten} gemeenten ingelezen")
+    return gemeenten
+
+gemeenten = lees_nis_bestand()
+naam = input("Geef gemeentenaam: ")
+code = gemeenten[naam.upper()]
+print(f"Gemeente {naam} heeft als NIS code {code}")
