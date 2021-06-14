fef4de94b4b6564cc1998b8f2860d6a43d63a3ef
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 06:31:42 +0200

    Print de gegevens van Anderlecht
    We lezen het CSV-bestand op de 'eenvoudige' manier. De eerste vier
    regels slaan we over, want die bevatten
    - de headers
    - de gegevens van België
    - de gegevens van het Brussels gewest
    - de gegevens van het arrondissement Brussel
    
    Met de split()-functie van string kunnen we de vijfde regel splitsen.
    Als scheidingsteken gebruiken we de puntkomma. Het resultaat bevat
    veel lege regels omdat er veel lege velden zijn op het einde van een
    record (regel).

--- niscodes.py
+++ niscodes.py
@@ -1,5 +1,10 @@
 BESTANDSNAAM = '../REFNIS_2019.csv'
 
 with open(BESTANDSNAAM, mode='r', encoding='utf_8_sig') as csvfile:
-    regel = csvfile.readline()
-    print(regel)
+    # We gebruiken '_' voor variabelen waarvan we de waarde niet gebruiken
+    # Op de vijfde regel staat de eerste echte gemeente
+    for _ in range(5):
+        regel = csvfile.readline()
+    items = regel.split(';')  # scheidingsteken is ';'
+    for item in items:
+        print(item)           # print de gegevens van Anderlecht
