f8d615807ab328cf743cc6ea69fbadee41a97400
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 06:54:40 +0200

    Gebruik veldnamen om CSV-bestand te lezen
    Omdat we een DictReader gebruiken, kunnen we de veldnamen gebruiken
    voor de verschillende velden. Het is best om hiervoor constanten te
    definiëren (NIS_CODE, GEMEENTENAAM). Zo voorkomen we typfouten
    
    Uiteindelijk zullen we een for-lus gebruiken om de verschillende
    records te lezen. Maar in dit voorbeeld gebruiken we de next()-functie.
    De next()-functie verwacht een iterator en geeft het volgende element
    van de iterator terug.
    
    Zie:
    - https://docs.python.org/3/library/functions.html#next

--- niscodes.py
+++ niscodes.py
@@ -1,8 +1,11 @@
 import csv
 
 BESTANDSNAAM = '../REFNIS_2019.csv'
-
+NIS_CODE = 'Code NIS'
+GEMEENTENAAM = 'Administratieve eenheden'
 
 with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
-    print(reader.fieldnames)
+    # de next() functie gaat naar het volgende item van de iterator
+    record = next(reader) 
+    print(record[NIS_CODE], record[GEMEENTENAAM])
