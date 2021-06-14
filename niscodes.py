8cb9c18e0a1849961de81a71dc15564a8c1aa842
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 06:18:37 +0200

    Hou rekening met UTF-8
    In de vorige versie zagen we dat het inlezen niet volledig lukte. Dat
    had te maken met de encoding van het bestand. We moeten aangeven dat
    het een UTF-8 bestand is.
    
    Er is ook een Byte-Order-Mark aanwezig (BOM). Vandaar dat we aan de
    standaard utf_8 encoding 'sig' (van signature) toevoegen.
    
    Zie:
    - https://docs.python.org/3/library/functions.html#open
    - https://en.wikipedia.org/wiki/Byte_order_mark
    - https://docs.python.org/3/library/codecs.html#standard-encodings

--- niscodes.py
+++ niscodes.py
@@ -1,5 +1,5 @@
 BESTANDSNAAM = '../REFNIS_2019.csv'
 
-with open(BESTANDSNAAM, mode='r') as csvfile:
+with open(BESTANDSNAAM, mode='r', encoding='utf_8_sig') as csvfile:
     regel = csvfile.readline()
     print(regel)
