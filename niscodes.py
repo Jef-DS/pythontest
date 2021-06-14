6f4ae353209c11c8a38bdccaa498f2093ddc9514
Joske Vermeulen <Joske.Vermeulen@schoten.be>
2021-06-14 07:08:22 +0200

    Tel het aantal gemeenten (de juiste versie)
    Het probleem met de vorige versie zat bij de provincies Vlaams-Brabant
    en Waals-Brabant. Vroeger was er 1 provincie Brabant met NIS-code
    20000. Bij de splitsing van de provincie heeft men die uitgesplits in
    20001 en 20002. Daarom klopte onze test op record[NIS_CODE][-3:]='000'
    niet voor de niet-gemeenten.
    
    Zie (voor de geïnteresseerden)
    - https://nl.wikipedia.org/wiki/Brabant_(provincie)
    - https://nl.wikipedia.org/wiki/Kieskring_Brussel-Halle-Vilvoorde

--- niscodes.py
+++ niscodes.py
@@ -7,12 +7,26 @@ GEMEENTENAAM = 'Administratieve eenheden'
 with open(BESTANDSNAAM, mode='r', newline='', encoding='utf_8_sig') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     totaal_aantal = 0
+    het_rijk = 0
+    aantal_gewesten = 0
+    aantal_provincies = 0
+    aantal_arrondissementen = 0
     aantal_gemeenten = 0
     for record in reader:
         totaal_aantal = totaal_aantal + 1
-        if record[NIS_CODE][-3:] == '000':
-            print(record[NIS_CODE], record[GEMEENTENAAM])
+        if record[NIS_CODE] == '01000':
+            het_rijk = het_rijk + 1
+        elif record[NIS_CODE][0] == '0':
+            aantal_gewesten = aantal_gewesten + 1
+        elif record[NIS_CODE][-4:-1] == '000':
+            aantal_provincies = aantal_provincies + 1
+        elif record[NIS_CODE][-3:] == '000':
+            aantal_arrondissementen = aantal_arrondissementen + 1
         else:
             aantal_gemeenten = aantal_gemeenten + 1
     print("Totaal:", totaal_aantal)
-    print("Gemeenten", aantal_gemeenten)
+    print("Het rijk:", het_rijk)
+    print("Aantal gewesten:", aantal_gewesten)
+    print("Aantal provincies: ", aantal_provincies)
+    print("Aantal arrondissementen: ", aantal_arrondissementen)
+    print("Aantal gemeenten:", aantal_gemeenten)
